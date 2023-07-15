c3773c8340a1c4c12cc3cd613ff21c1a3fb0361d
ankugargdev619 <137602411+ankugargdev619@users.noreply.github.com>
2023-07-15 15:31:00 +0530

    changes to implement color change on scroll
    The color of the body will change based on the section being viewed

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-$(window).scroll(function() {
-  
-    // selectors
-    var $window = $(window),
-        $body = $('body'),
-        $panel = $('.issue');
-    
-    // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
-   
-    $panel.each(function () {
-      var $this = $(this);
-      
-      // if position is within range of this panel.
-      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-      // Remember we set the scroll to 33% earlier in scroll var.
-      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-        // Remove all classes on body with color-
-        $body.removeClass(function (index, css) {
-          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-        });
-         
-        // Add class of currently active div
-        $body.addClass('color-' + $(this).data('color'));
-      }
-    });    
-    
-  }).scroll();
+const issues = document.querySelectorAll('.issue');
+
+const body = document.querySelector('body');
+
+console.log(body);
+
+const options = {
+    threshold:0.5,
+}
+
+const observer = new IntersectionObserver(sections =>{
+    sections.forEach(section=>{
+        if(section.isIntersecting){
+            const color = section.target.getAttribute('data-color');
+            console.log(color);
+            body.removeAttribute('class');
+            body.classList.add(color);
+
+        }
+    })
+},options);
+
+issues.forEach(issue=>{
+    observer.observe(issue);
+})
